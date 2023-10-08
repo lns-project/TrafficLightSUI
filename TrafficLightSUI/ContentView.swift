b1afd786f126b058134950d058c70cb0539bd9fd
Ginza31 <lns-project@yandex.ru>
2023-10-08 15:14:10 +0300

    Initial Commit

---
+++ TrafficLightSUI/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  TrafficLightSUI
+//
+//  Created by Динара Шарафутдинова on 08.10.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TrafficLightSUI/TrafficLightSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightSUIApp.swift
+//  TrafficLightSUI
+//
+//  Created by Динара Шарафутдинова on 08.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
